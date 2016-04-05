202c2648d638fa2f86e7643a94a290c4c35b4af4
Kyle Cierzan <kcierzan@gmail.com>
2016-04-04 18:03:29 -0700

    Add past trips page

--- app/Components/PastTrips.js
+++ app/Components/PastTrips.js
@@ -9,15 +9,82 @@ var {
   TextInput
 } = React;
 
-class PastTrips extends React.Component{
+var styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    flexDirection: 'column',
+  },
+  buttonText: {
+    fontSize: 18,
+    color: 'white'
+  },
+  button: {
+    height: 60,
+    backgroundColor: '#48BBEC',
+    flex: 3,
+    alignItems: 'center',
+    justifyContent: 'center'
+  },
+  searchInput: {
+    height: 60,
+    padding: 10,
+    fontSize: 18,
+    color: '#111',
+    flex: 10
+  },
+  rowContainer: {
+    padding: 10,
+  },
+  footerContainer: {
+    backgroundColor: '#E3E3E3',
+    alignItems: 'center',
+    flexDirection: 'row'
+  }
+});
 
-  render(){
+class PastTrips extends React.Component{
+  constructor(props){
+    super(props);
+    this.ds = new Listview.DataSource({rowHasChanged: (row1, row2) => row1 !== row2})
+    this.state = {
+      dataSource: this.ds.cloneWithRows(this.props.notes),
+      trip: '',
+      error: ''
+    }
+  }
+  handleChange(e){
+    this.setState({
+      trip: e.nativeEvent.text
+    })
+  }
+  handleSubmit(){
+    var trip = this.state.trip;
+    this.setState({
+      trip: ''
+    });
+    api.addTrip(this.props.userName, trip)
+      .then((data)=> {
+        api.getTrips(this.props.userName)
+          .then((data)=> {
+            this.setState({
+              dataSource:this.ds.cloneWithRows(data)
+            })
+          });
+      })
+      .catch((error)=> {
+        console.log('Request failed', error);
+        this.setState({error})
+      });
+  }
+  renderRow(rowData){
     return (
-      <View style={styles.mainContainer}>
-        <Text>Boooooobs</Text>
+      <View>
+        <View style={styles.rowContainer}>
+          <Text> {rowData} </Text>
       </View>
     )
   }
-}
+};
+
 
 module.exports = PastTrips;
