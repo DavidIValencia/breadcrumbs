b83e69af6a15697b9360f377491033a9168985a1
jjschnei <jeffjschneider@gmail.com>
2016-04-05 17:01:02 -0700

    Experiment with log in page

--- app/Components/Main.js
+++ app/Components/Main.js
@@ -12,48 +12,102 @@ var {
   AlertIOS,
   ActivityIndicatorIOS,
   TextInput
+  
 } = React;
 
 var styles = StyleSheet.create({
-  button: {
-    opacity: 0.5,
-    height: 45,
-    flexDirection: 'row',
-    backgroundColor: 'black',
-    borderColor: 'black',
-    borderWidth: 1,
-    borderRadius: 8,
-    marginBottom: 10,
-    marginTop: 10,
-    marginRight: 50,
-    marginLeft: 50,
-    alignSelf: 'stretch',
-    justifyContent: 'center',
-  },
-  mainContainer: {
+container: {
+    flexDirection: 'column',
     flex: 1,
-    justifyContent: 'flex-start',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-    padding: 10,
-    paddingTop: 80
+    backgroundColor: 'transparent'
   },
-  input: {
-    height: 50,
-    marginTop: 10,
-    padding: 4,
-    fontSize: 18,
-    borderColor: 'black',
-    borderWidth: 1,
-    borderRadius: 8,
-    marginBottom: 10,
-    marginTop: 10,
-    marginRight: 50,
-    marginLeft: 50,
-    color: 'white',
-    backgroundColor: 'black',
-    opacity: 0.5,
-
+  bg: {
+      position: 'absolute',
+      left: 0,
+      top: 0,
+      width: windowSize.width,
+      height: windowSize.height
+  },
+  header: {
+      justifyContent: 'center',
+      alignItems: 'center',
+      flex: .5,
+      backgroundColor: 'transparent'
+  },
+  mark: {
+      width: 150,
+      height: 150
+  },
+  signin: {
+      backgroundColor: '#FF3366',
+      padding: 20,
+      alignItems: 'center'
+  },
+  signup: {
+    justifyContent: 'center',
+    alignItems: 'center',
+    flex: .15
+},
+button: {
+  opacity: 0.5,
+  height: 45,
+  flexDirection: 'row',
+  backgroundColor: 'black',
+  borderColor: 'black',
+  borderWidth: 1,
+  borderRadius: 8,
+  marginBottom: 10,
+  marginTop: 10,
+  marginRight: 50,
+  marginLeft: 50,
+  alignSelf: 'stretch',
+  justifyContent: 'center',
+},
+mainContainer: {
+  flex: 1,
+  justifyContent: 'flex-start',
+  alignItems: 'center',
+  backgroundColor: '#F5FCFF',
+  padding: 10,
+  paddingTop: 80
+},
+input: {
+  height: 50,
+  marginTop: 10,
+  padding: 4,
+  fontSize: 18,
+  borderColor: 'black',
+  borderWidth: 1,
+  borderRadius: 8,
+  marginBottom: 10,
+  marginTop: 10,
+  marginRight: 50,
+  marginLeft: 50,
+  color: 'white',
+  backgroundColor: 'black',
+  opacity: 0.5,
+  // position: 'absolute',
+  // left: 61,
+  // top: 12,
+  // right: 0,
+  // height: 20,
+  // fontSize: 14
+},
+inputPassword: {
+  marginLeft: 15,
+  width: 20,
+  height: 21
+},
+  inputUsername: {
+    marginLeft: 15,
+    width: 20,
+    height: 20
+  },
+  inputContainer: {
+      padding: 10,
+      borderWidth: 1,
+      borderBottomColor: '#CCC',
+      borderColor: 'transparent'
   },
 
   title: {
@@ -62,7 +116,21 @@ var styles = StyleSheet.create({
     textAlign: 'center',
     color: 'white'
   },
-
+  inputs: {
+    marginTop: 10,
+    marginBottom: 10,
+    flex: .25
+  },
+  forgotContainer: {
+    alignItems: 'flex-end',
+    padding: 15,
+  },
+  greyFont: {
+    color: '#D8D8D8'
+  },
+  whiteFont: {
+    color: '#FFF'
+  }
   backgroundImage: {
     flex: 1,
     resizeMode: 'stretch',
